0e8fb8bb61b0b9b7f62ce5ae59f95ad01a532d7e
sugilog <sugilog@gmail.com>
2017-06-30 21:50:24 +0900

    test rfc

--- formatter/filter_test.go
+++ formatter/filter_test.go
@@ -7,28 +7,25 @@ import (
 )
 
 func TestFilterFormatter(t *testing.T) {
+	line := "test:line\tvalid:true"
+
 	tests := []struct {
-		Line   string
 		Keys   []string
 		Expect string
 	}{
 		{
-			Line:   "test:line\tvalid:true",
 			Keys:   []string{"test", "valid"},
 			Expect: "test:line\tvalid:true",
 		},
 		{
-			Line:   "test:line\tvalid:true",
 			Keys:   []string{"test"},
 			Expect: "test:line",
 		},
 		{
-			Line:   "test:line\tvalid:true",
 			Keys:   []string{"Test", "valid"},
 			Expect: "valid:true",
 		},
 		{
-			Line:   "test:line\tvalid:true",
 			Keys:   []string{""},
 			Expect: "",
 		},
@@ -36,51 +33,48 @@ func TestFilterFormatter(t *testing.T) {
 
 	for _, test := range tests {
 		formatter := FilterFormatter(test.Keys)
-		entry, ok := formatter(io.Entry{Line: test.Line})
+		entry, ok := formatter(io.Entry{Line: line})
 
 		if !ok {
 			t.Error("Should be ok")
 		}
 
 		if test.Expect != entry.Formatted {
-			t.Error("<%s> expected, but was <%s>", test.Expect, entry.Formatted)
+			t.Errorf("<%s> expected, but was <%s>", test.Expect, entry.Formatted)
 		}
 	}
 }
 
 func TestFilterFunc(t *testing.T) {
+	ltsv := map[string]string{"test": "line", "valid": "true"}
+
 	tests := []struct {
-		LTSV   map[string]string
 		Keys   []string
 		Expect string
 	}{
 		{
-			LTSV:   map[string]string{"test": "line", "valid": "true"},
 			Keys:   []string{"test", "valid"},
 			Expect: "test:line\tvalid:true",
 		},
 		{
-			LTSV:   map[string]string{"test": "line", "valid": "true"},
 			Keys:   []string{"test"},
 			Expect: "test:line",
 		},
 		{
-			LTSV:   map[string]string{"test": "line", "valid": "true"},
 			Keys:   []string{"Test", "valid"},
 			Expect: "valid:true",
 		},
 		{
-			LTSV:   map[string]string{"test": "line", "valid": "true"},
 			Keys:   []string{""},
 			Expect: "",
 		},
 	}
 
 	for _, test := range tests {
-		converted := FilterFunc(test.LTSV, test.Keys)
+		converted := FilterFunc(ltsv, test.Keys)
 
 		if test.Expect != converted {
-			t.Error("<%s> expected, but was <%s>", test.Expect, converted)
+			t.Errorf("<%s> expected, but was <%s>", test.Expect, converted)
 		}
 	}
 }
