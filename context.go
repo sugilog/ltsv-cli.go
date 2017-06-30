dd812e7da52e6b3f888cd675700c9551d7b7d3c3
sugilog <sugilog@gmail.com>
2017-06-30 14:13:01 +0900

    filter

--- context.go
+++ context.go
@@ -2,42 +2,55 @@ package lc
 
 import (
 	"errors"
-	"regexp"
+	"fmt"
+	// "regexp"
+	"os"
 	"strings"
 
 	"github.com/codegangsta/cli"
+	"github.com/sugilog/ltsv-cli.go/formatter"
+	"github.com/sugilog/ltsv-cli.go/io"
 )
 
-func Word(context *cli.Context) (*regexp.Regexp, error) {
-	if len(context.Args()) == 0 {
-		return nil, errors.New("Word not given")
+// no test
+func Filter(context *cli.Context) {
+	keys, err := Keys(context)
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
 
-	word := context.Args()[0]
-
-	if len(word) <= 0 {
-		return nil, errors.New("Word not given")
-	} else {
-		return regexp.MustCompile(word), nil
+	filter := formatter.FilterFormatter(keys)
+	iomap := io.IOMap{
+		Out: os.Stdout,
+		Err: os.Stderr,
+		In:  os.Stdin,
 	}
+	io.Worker(iomap, filter)
 }
 
-func Keys(context *cli.Context) (map[string]bool, error) {
+// func Word(context *cli.Context) (*regexp.Regexp, error) {
+// 	if len(context.Args()) == 0 {
+// 		return nil, errors.New("Word not given")
+// 	}
+
+// 	word := context.Args()[0]
+
+// 	if len(word) <= 0 {
+// 		return nil, errors.New("Word not given")
+// 	} else {
+// 		return regexp.MustCompile(word), nil
+// 	}
+// }
+
+func Keys(context *cli.Context) ([]string, error) {
 	arg := context.String("key")
-	mapped := make(map[string]bool)
 	splitted := strings.Split(arg, ",")
 
 	if len(splitted) == 1 && splitted[0] == "" {
-		return mapped, errors.New("Key(s) not given")
-	}
-
-	for _, key := range splitted {
-		mapped[key] = true
-	}
-
-	if len(mapped) > 0 {
-		return mapped, nil
+		return splitted, errors.New("Key(s) not given")
 	} else {
-		return mapped, errors.New("Key(s) not given")
+		return splitted, nil
 	}
 }
--- context_test.go
+++ context_test.go
@@ -7,44 +7,44 @@ import (
 	"github.com/codegangsta/cli"
 )
 
-func TestWord(t *testing.T) {
-	set := flag.NewFlagSet("test", 0)
-	c := cli.NewContext(nil, set, nil)
+// func TestWord(t *testing.T) {
+// 	set := flag.NewFlagSet("test", 0)
+// 	c := cli.NewContext(nil, set, nil)
 
-	set.Parse([]string{"grep word", "--key", "field1"})
+// 	set.Parse([]string{"grep word", "--key", "field1"})
 
-	pattern, err := Word(c)
+// 	pattern, err := Word(c)
 
-	if err != nil {
-		t.Error(err)
-	}
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
 
-	if !pattern.MatchString("Is there grep word ??") {
-		t.Error("Word should compile 'grep word'", pattern)
-	}
+// 	if !pattern.MatchString("Is there grep word ??") {
+// 		t.Error("Word should compile 'grep word'", pattern)
+// 	}
 
-	if pattern.MatchString("Is there rep word ??") {
-		t.Error("Word should compile 'grep word'", pattern)
-	}
-}
+// 	if pattern.MatchString("Is there rep word ??") {
+// 		t.Error("Word should compile 'grep word'", pattern)
+// 	}
+// }
 
-func TestWordWithEmptyArgument(t *testing.T) {
-	set := flag.NewFlagSet("test", 0)
-	c := cli.NewContext(nil, set, nil)
+// func TestWordWithEmptyArgument(t *testing.T) {
+// 	set := flag.NewFlagSet("test", 0)
+// 	c := cli.NewContext(nil, set, nil)
 
-	var empty []string
-	set.Parse(empty)
+// 	var empty []string
+// 	set.Parse(empty)
 
-	_, err := Word(c)
+// 	_, err := Word(c)
 
-	if err == nil {
-		t.Error("Should return error")
-	}
+// 	if err == nil {
+// 		t.Error("Should return error")
+// 	}
 
-	if err.Error() != "Word not given" {
-		t.Error("Error Message should say 'Word not given'")
-	}
-}
+// 	if err.Error() != "Word not given" {
+// 		t.Error("Error Message should say 'Word not given'")
+// 	}
+// }
 
 func TestKeys(t *testing.T) {
 	set := flag.NewFlagSet("test", 0)
